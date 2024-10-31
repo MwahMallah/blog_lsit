bea6f0035c89d824fbf3ee16f4638232c30585fe
MwahMallah <maximus27rus@gmail.com>
2024-10-31 20:10:40 +0100

    blogs route now uses async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,21 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get("/", (req, res) => {
-    Blog.find({})
-        .then(result => res.json(result));
+blogsRouter.get("/", async (req, res) => {
+    const blogs = await Blog.find({});
+    res.json(blogs);
 });
 
 blogsRouter.post('/', (req, res) => {
     const blog = new Blog(req.body)
-  
+
     blog
-        .save()
-        .then(result => {
-            res.status(201).json(result);
-        })
-  })
+    .save()
+    .then(result => {
+        res.status(201).json(result);
+    })
+})
+
+
 
 module.exports = blogsRouter;
