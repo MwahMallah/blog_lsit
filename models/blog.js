c798dcd7b5cdf67e8ca4540d3e35f151768f8045
MwahMallah <maximus27rus@gmail.com>
2024-10-31 20:43:22 +0100

    refactored tests to use helper, added some test. Url is now required in blog scheme.

--- models/blog.js
+++ models/blog.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
     title: {type: String},
     author: {type: String},
-    url: {type: String},
-    likes: {type: Number}
+    url: {type: String, required: true},
+    likes: {type: Number, default: 0}
 });
 
 blogSchema.set('toJSON', {
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -4,57 +4,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog');
-
-const testBlogs = [
-    {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-    },
-    {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-    },
-    {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-    },
-    {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-    },
-    {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-    },
-    {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-    }  
-]
+const {testBlogs, blogsInDb} = require('./test_helper');
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -65,9 +15,7 @@ beforeEach(async () => {
     }
 });
 
-
 const api = supertest(app)
-
 
 test.only('Blogs are returned as json', async () => {
     await api
@@ -90,15 +38,49 @@ test.only('blogs have id and don\'t have _id', async () => {
 });
 
 test.only('blogs increased by one, when there is post request', async () => {
+    const newBlog = {
+        title: "new title",
+        author: "new author",
+        url: "new url",
+        likes: 5,
+    };
 
     const response = await api.post('/api/blogs')
                         .send(newBlog)
                         .expect(201);
-  
-    assert.notStrictEqual(response.body[0].id, undefined)
-    assert.strictEqual(response.body[0]._id, undefined)
+    
+    const { id, ...savedBlogWithoutId } = response.body;
+    assert.strictEqual((await blogsInDb()).length, 7);
+    assert.deepStrictEqual(savedBlogWithoutId, newBlog);
+});
+
+test.only('blog likes are set to 0, when there is no likes in post request', async () => {
+    const newBlog = {
+        title: "new title",
+        author: "new author",
+        url: "new url"
+    };
+
+    const response = await api.post('/api/blogs')
+                        .send(newBlog)
+                        .expect(201);
+    
+    const savedBlog = response.body;
+    assert.strictEqual(savedBlog.likes, 0);
+});
+
+test.only('posting blog without url results in response code 400', async () => {
+    const newBlog = {
+        title: "new title",
+        author: "new author",
+        likes: 5,
+    };
+
+    const response = await api.post('/api/blogs')
+                        .send(newBlog)
+                        .expect(400);
 });
   
 after(async () => {
-    await mongoose.connection.close()
+    await mongoose.connection.close();
 })
